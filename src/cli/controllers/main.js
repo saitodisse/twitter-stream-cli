6bf91cdc72860e4c0871ab833e59751f78f1e8cf
Julio Makdisse Saito <saitodisse@gmail.com>
2016-07-23 16:07:53 -0300

    [project] update eslint, firebase and packages

--- src/cli/controllers/main.js
+++ src/cli/controllers/main.js
@@ -3,6 +3,7 @@ import Main from '../../main';
 import Saver from '../../saver';
 import Formatter from '../../formatter';
 import c from 'chalk';
+import firebase from 'firebase';
 
 class MainController extends CliController {
   index(params) {
@@ -13,11 +14,18 @@ class MainController extends CliController {
     const formatter = new Formatter(params);
     const willSendToFirebase = params.send;
 
+    let db;
+
     console.error(c.gray('-----------------'));
     console.error('# Tracking tweets:', trackWords);
     console.error('# Languages:', languages);
     if (params.send) {
       console.error('# Firebase repository:', process.env.FIREBASE_URL);
+      firebase.initializeApp({
+        apiKey: process.env.FIREBASE_APIKEY,
+        databaseURL: process.env.FIREBASE_DATABASEURL,
+      });
+      db = firebase.database();
     }
     console.error(c.gray('-----------------'));
 
@@ -26,7 +34,7 @@ class MainController extends CliController {
         formatter.format([tweet]).map((line) => console.log(line));
         if (willSendToFirebase) {
           const saver = new Saver();
-          saver.save(trackWords, tweet);
+          saver.save(db, trackWords, tweet);
         }
       }
     });
--- src/saver/index.js
+++ src/saver/index.js
@@ -1,20 +1,14 @@
 import _ from 'lodash';
-const Firebase = require('firebase');
 
 class Saver {
   constructor(opts) {
     this._opts = _.merge({}, opts);
-    // TODO: change this so we can receive FIREBASE_URL as cli option
-    this.ref = new Firebase(process.env.FIREBASE_URL);
   }
 
-  save(trackWords, tweet) {
-    const tweetsRef = this.ref.child('tweets');
-
-    const itemPath = _.camelCase(trackWords.join(' '));
-    const itemRef = tweetsRef.child(itemPath);
-
-    itemRef.push().set(tweet);
+  save(db, trackWords, tweet) {
+    // const tweetsRefPush = this.ref.child('itens');
+    const ref = db.ref('tweets/' + trackWords);
+    ref.push().set(tweet);
   }
 }
 
